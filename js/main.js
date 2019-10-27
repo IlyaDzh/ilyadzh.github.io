517a58a5ab23d7110fe02190ada73c84dc964bd5
Илья Долженко <ilay00@mail.ru>
2019-10-27 16:11:40 +0300

    add space title

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.z = 8;
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshLambertMaterial({ color: 0xffcc00 });
 
-for (let i = 0; i < 35; i++) {
+for (let i = 0; i < 40; i++) {
     var mesh = new THREE.Mesh(geometry, material);
-    mesh.position.x = (Math.random() - 0.5) * 13;
-    mesh.position.y = (Math.random() - 0.5) * 13;
-    mesh.position.z = (Math.random() - 0.5) * 9;
+    mesh.position.x = (Math.random() - 0.5) * 15;
+    mesh.position.y = (Math.random() - 0.5) * 15;
+    mesh.position.z = (Math.random() - 0.5) * 10;
+    mesh.rotation.y = Math.random() * Math.PI;
+    mesh.rotation.x = Math.random() * Math.PI;
     scene.add(mesh);
 }
 
-var light = new THREE.PointLight(0xffffff, 1, 500);
-light.position.set(0, 0, 25);
+var light = new THREE.PointLight(0xffffff, 1, 1000);
+light.position.set(0, 0, 15);
 scene.add(light);
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -31,9 +33,4 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
-animate();
+renderer.render(scene, camera);
