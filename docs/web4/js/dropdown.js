eada8ebda9a65e737388087ec34df3d39c466609
Илья Долженко <ilay00@mail.ru>
2019-11-11 15:55:41 +0300

    space

--- docs/web4/js/dropdown.js
+++
@@ -1,7 +0,0 @@
-$("#drop").mouseover(() => {
-    $("#menu-list").addClass('d-block');
-});
-
-$("#drop").mouseout(() => {
-    $("#menu-list").removeClass('d-block');
-});
--- docs/web4/js/modal.js
+++
@@ -1,9 +0,0 @@
-var modal = $('.cover, .modal, .modal__content');
-
-$('.modal').click(()=> {
-    modal.fadeOut();
-});
-
-$('.btn-danger').click(() => {
-    modal.fadeIn();
-});
